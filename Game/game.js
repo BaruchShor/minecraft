c8c4c2633afcfd481b1b8256df6ceb8e37db13a9
Hershi Shor <e0548597918@gmail.com>
2025-08-20 15:24:07 +0300

    created the world with html and css

--- Game/game.js
+++ Game/game.js
@@ -1,6 +1,31 @@
 const grid = document.getElementById("gameGrid");
 
-for (let i = 0; i <= 11000; i++) {
+for (let i = 0; i <= 1199; i++) {
   const card = document.createElement("section");
+  card.className = "sky";
+  grid.append(card);
+}
+
+for (let i = 0; i <= 99; i++) {
+  const card = document.createElement("section");
+  card.className = "ground";
+  grid.append(card);
+}
+
+for (let i = 0; i <= 599; i++) {
+  const card = document.createElement("section");
+  card.className = "dirt";
+  grid.append(card);
+}
+
+for (let i = 0; i <= 1599; i++) {
+  const card = document.createElement("section");
+  card.className = "stone";
+  grid.append(card);
+}
+
+for (let i = 0; i <= 499; i++) {
+  const card = document.createElement("section");
+  card.className = "bedroce";
   grid.append(card);
 }
