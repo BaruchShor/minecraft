67f0af0921f482d117443db49bf28cfcc52b1e82
Chaim Rozen <czr770@gmail.com>
2025-08-20 15:07:01 +0300

    tree in work

--- Game/game.js
+++ Game/game.js
@@ -1,6 +1,25 @@
 const grid = document.getElementById("gameGrid");
+const gridW = 100;
+const gridH = 30;
 
-for (let i = 0; i <= 11000; i++) {
+for (let i = 0; i <gridW * gridH; i++) {
   const card = document.createElement("section");
   grid.append(card);
+}
+
+// for (let i = 0; i <= 11000; i++) {
+//   const card = document.createElement("section");
+//   grid.append(card);
+// }
+
+const allCells = document.querySelectorAll("#gameGrid section");
+
+// allCells.forEach((cell, index) => {
+//   if (index < 1008) {        // רק התאים עם אינדקס 0 עד 99
+//     cell.style.backgroundColor = "red";
+//   }
+// });
+
+function createTree(x, y){
+  
 }
